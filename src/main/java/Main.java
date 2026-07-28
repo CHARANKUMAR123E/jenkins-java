ff89c50183c220e25efaa60889fa63a2d2340055
CHARANKUMAR123E <charankumar2772003@gmail.com>
2026-07-28 18:20:33 +0530

    Updated application message for webhook testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("=================================");
-        System.out.println(" Welcome to Jenkins CI Pipeline ");
+        System.out.println(" Welcome to Jenkins CI Pipeline Version 2");
         System.out.println("=================================");
 
         System.out.println("Application Started Successfully");
