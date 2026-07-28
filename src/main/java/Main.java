5f27d3d75edf79f48aa9021d3dca122d4ba842eb
CHARANKUMAR123E <charankumar2772003@gmail.com>
2026-07-28 18:26:38 +0530

    Upated the application to Version 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("=================================");
-        System.out.println(" Welcome to Jenkins CI Pipeline Version 2");
+        System.out.println(" Welcome to Jenkins CI Pipeline Version 3");
         System.out.println("=================================");
 
         System.out.println("Application Started Successfully");
